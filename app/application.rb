a2c17cf82c051185ade1d7c49708997e2a6addef
Monica  Karasek <mlkarasek@gmail.com>
2018-08-08 23:32:07 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,11 +1,11 @@
 class Application
-  
+
   @@items = []
- 
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
- 
+
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find {|i| i.name == item_name}
